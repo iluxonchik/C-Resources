c0adb9c54c4eaa564d524479becbcf72859528b1
iluxonchik <iluxon4ik@hotmail.com>
2014-09-16 21:07:11 +0100

    Additions to the Animal.

--- Examples/Animal/Animal.c
+++ Examples/Animal/Animal.c
@@ -10,6 +10,11 @@ Implemente a função newAnimal que reserva memória suficiente para representar um
 os seus campos (através dos argumentos da função). Por simplicidade, assuma que o campo _name tem comprimento
 fixo máximo de 16 caracteres (incluindo o terminador). Implemente também a função destroyAnimal, que liberta
 os recursos associados ao animal.
+
+a. Implemente a função de comparação - equalsAnimal -, por forma a considerar que dois animais são iguais se as suas características forem iguais.
+a. Implemente funções de acesso às variáveis de um animal: getAnimalName, getAnimalAge, getAnimalWeight (dado um animal, retornam um dos seus campos).
+a. Implemente a função printAnimal que, quando aplicada a um animal, apresenta os seus dados (use printf para apresentar cada campo do animal).
+a. Implemente um programa - main - que ilustre a utilização das funções anteriores.
 */
 
 typedef struct animal{
@@ -35,4 +40,24 @@ Animal* newAnimal(char* name, int age, int weight){
 		animal->_name[nameLen] = '\0';
 	}
 	return animal;
+}
+
+void destroyAnimal(Animal* animal){
+	if (animal != NULL){
+		// First, free the string (name)
+		if (animal->_name != NULL){
+			free(animal->_name);
+		}
+		free(animal);
+	}
+}
+
+int equalsAnimal(Animal* animal1, Animal* animal2){
+	if (animal1 != NULL && animal2 != NULL){
+		if (animal1->_age == animal2->_age && animal1->_weight == animal2->_weight && !(strcmp(animal1->_name, animal2->_name)))
+			return 1;
+		else
+			return 0;
+	}
+	return -1; // something went wrong
 }
--- Examples/Animal/Animal.h
+++ Examples/Animal/Animal.h
@@ -1,1 +1,2 @@
 Animal* newAnimal(char* name, int age, int weight);
+int equalsAnimal(Animal* animal1, Animal* animal2);
